56122a6319fe41450e46d9de0674e205b69c9393
IGNACIO ROBLES <roblesignacio7@gmail.com>
2019-04-23 21:40:18 -0400

    contactos

---
+++ app/src/main/java/com/jellybeans/Contacto.java
@@ -0,0 +1,47 @@
+package com.jellybeans;
+
+public class Contacto {
+    String nombre;
+    String apellido;
+    String correo;
+    int telefono;
+
+    public Contacto(String nombre, String apellido, String correo, int telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
