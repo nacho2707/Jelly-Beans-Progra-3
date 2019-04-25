aa767005895f0b72e4e272bb5ec9d818a526222a
danifernadezq <daniel.fq11@gmail.com>
2019-04-25 19:24:04 -0400

    Intent para pasar de login a main act

---
+++ app/src/main/java/com/jellybeans/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.jellybeans;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void doLogin(View view){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
