bf30bad443e6034a594d50aa2f3e12bb949687d9
danifernadezq <daniel.fq11@gmail.com>
2019-04-28 18:38:33 -0400

    Validaciones y correcion de layout rutas, layout horarios, (background, textSize)

--- app/src/main/java/com/jellybeans/LoginActivity.java
+++ app/src/main/java/com/jellybeans/LoginActivity.java
@@ -3,11 +3,15 @@ package com.jellybeans;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText editTextCodigo;
+    EditText editTextContraseña;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +19,46 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void doLogin(View view){
+        editTextCodigo= (EditText) findViewById(R.id.editTextCodigo);
+        editTextContraseña= (EditText) findViewById(R.id.editTextContraseña);
+        /*Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);*/
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        validar();
     }
+
+    public void validar(){
+        editTextContraseña.setError(null);
+        editTextCodigo.setError(null);
+
+        String codigo= editTextCodigo.getText().toString();
+        String contraseña= editTextContraseña.getText().toString();
+
+        if(TextUtils.isEmpty(codigo)){
+            editTextCodigo.setError("Campo obligatorio");
+            editTextCodigo.requestFocus();
+            return;
+        }
+
+        if(TextUtils.isEmpty(contraseña)){
+            editTextCodigo.setError("Campo obligatorio");
+            editTextCodigo.requestFocus();
+            return;
+        }
+
+
+
+        if(codigo.length() != 5 ){
+            editTextCodigo.setError("Codigo de 5 digitos");
+            editTextCodigo.requestFocus();
+            return;
+        }
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
 
 
 
