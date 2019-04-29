4c6ab1046de0062e3ea14fd85a4261c04c6fdcd2
IGNACIO ROBLES <roblesignacio7@gmail.com>
2019-04-28 22:19:27 -0400

    back mapa

--- app/src/main/java/com/jellybeans/MapActivity.java
+++ app/src/main/java/com/jellybeans/MapActivity.java
@@ -1,5 +1,6 @@
 package com.jellybeans;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,9 +9,19 @@ public class MapActivity extends AppCompatActivity {
     @Override
 
 
-
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_map);
+        setupActionBar();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Mapa");
+        }
+
     }
 }
